dd8f784c55b93ed742f57a8d4eaa6d1a3a5e14ed
GR333N893 <natalizedstolpinator@hotmail.com>
2025-09-27 23:38:50 -0600

    add main page

--- minigames/blackjack/blackjack.js
+++ minigames/blackjack/blackjack.js
@@ -1,16 +1,74 @@
 more.style.display = 'none';
 stand.style.display = 'none';
 pa.style.display = 'none';
-const deck = [];
-const discardPile = [];
+let deck = [];
+let discardPile = [];
+let playerHand = [];
+let dealerHand = [];
 
 function start() {
+    if (deck.length == 0 && discardPile.length == 0) {
+        let suit = [
+            "spades",
+            "hearts",
+            "diamonds",
+            "clubs"
+        ];
+        let faces = [
+            "jack",
+            "queen",
+            "king"
+        ];
+        for (let s = 0; s < 4; s++) {
+            let ace = {
+                face: "ace",
+                value: 11,
+                suit: suit[s],
+                back: "cards/back.png"
+            };
+            ace.front = "cards/" + ace.face + suit[s] + ".png";
+            deck.push(ace);
+
+            for (let v = 0; v < 9; v++) {
+                let cardValue = v + 2;
+                let card = {
+                    face: cardValue,
+                    value: cardValue,
+                    suit: suit[s],
+                    back: "cards/back.png"
+                };
+                card.front = "cards/" + card.face + suit[s] + ".png";
+                deck.push(card);
+            }
+
+            for (let f = 0; f < 3; f++) {
+                let royalty = {
+                    face: faces[f],
+                    value: 10,
+                    suit: suit[s],
+                    back: "cards/back.png"
+                };
+                royalty.front = "cards/" + royalty.face + suit[s] + ".png";
+                deck.push(royalty);
+            }
+        }
+    }
+
     button.style.display = 'none';
     dwipe();
     pwipe();
     pa.style.display = 'none';
     more.style.display = 'inline-block';
     stand.style.display = 'inline-block';
+
+    function rebuild() {
+        if (deck.length < 1) {
+            while (discardPile.length > 0) {
+                deck.push(discardPile[0]);
+                discardPile.splice(discardPile[0], 1);
+            };
+        }
+    }
 
     function dwipe() {
         dtotal = 0;
@@ -26,25 +84,24 @@ function start() {
 
     function draw() {
         let num = Math.floor(Math.random() * deck.length);
-        const randomCard = deck[num];
+        let randomCard = deck[num];
         deck.splice(num, 1);
-        discardPile.push(randomCard);
         return randomCard;
     }
 
     function plist() {
         playerHand.forEach(card => {
             ptotal += card.value;
-            const imgElement = document.createElement("img");
+            let imgElement = document.createElement("img");
             imgElement.src = card.front;
             imgElement.alt = card.face + " of " + card.suit;
             document.getElementById("3").appendChild(imgElement);
         });
         document.getElementById("4").innerHTML = ptotal;
 
-        if (ptotal >= 21) {
+        /*if (ptotal >= 21) {
             roundEnd();
-        }
+        }*/
     }
 
     function roundEnd() {
@@ -54,7 +111,7 @@ function start() {
         pa.style.display = 'inline-block';
 
         dealerHand.forEach(card => {
-            const imgElement = document.createElement("img");
+            let imgElement = document.createElement("img");
             dtotal += card.value;
             imgElement.src = card.front;
             imgElement.alt = card.face + " of " + card.suit;
@@ -62,91 +119,53 @@ function start() {
         });
 
         document.getElementById("1").innerHTML = dtotal;
+        document.getElementById("2.5").innerHTML = deck.length + " " + discardPile.length;
+
+        let playAgain = document.getElementById("pa");
+
+        playAgain.addEventListener("click", function() {
+            while (playerHand.length > 0) {
+                discardPile.push(playerHand[0]);
+                playerHand.splice(playerHand[0], 1);
+            }
+
+            while (dealerHand.length > 0) {
+                discardPile.push(dealerHand[0]);
+                dealerHand.splice(dealerHand[0], 1);
+            }
+
+            start();
+        });
     }
 
-    const hitBtn = document.getElementById("more");
+    let hitBtn = document.getElementById("more");
 
     hitBtn.addEventListener("click", function() {
         pwipe();
-        const card = draw();
+        rebuild();
+        let card = draw();
         playerHand.push(card);
-
+        document.getElementById("2.5").innerHTML = deck.length + " " + discardPile.length;
         plist();
     });
 
-    const standBtn = document.getElementById("stand");
+    let standBtn = document.getElementById("stand");
 
     standBtn.addEventListener("click", function() {
         roundEnd();
     });
 
-    if (deck.length < 1 && discardPile.length < 1) {
-        const suit = [
-            "spades",
-            "hearts",
-            "diamonds",
-            "clubs"
-        ];
-        const faces = [
-            "jack",
-            "queen",
-            "king"
-        ];
-        for (let s = 0; s < 4; s++) {
-            const ace = {
-                face: "ace",
-                value: 11,
-                suit: suit[s],
-                back: "cards/back.png"
-            };
-            ace.front = "cards/" + ace.face + suit[s] + ".png";
-            deck.push(ace);
-
-            for (let v = 0; v < 9; v++) {
-                const cardValue = v + 2;
-                const card = {
-                    face: cardValue,
-                    value: cardValue,
-                    suit: suit[s],
-                    back: "cards/back.png"
-                };
-                card.front = "cards/" + card.face + suit[s] + ".png";
-                deck.push(card);
-            }
-
-            for (let f = 0; f < 3; f++) {
-                const royalty = {
-                    face: faces[f],
-                    value: 10,
-                    suit: suit[s],
-                    back: "cards/back.png"
-                };
-                royalty.front = "cards/" + royalty.face + suit[s] + ".png";
-                deck.push(royalty);
-            }
-        }
-    }
-    else if (deck.length < 1) {
-        discardPile.forEach(card => {
-            deck.push(card);
-            discardPile.splice(card, 1);
-        });
-    }
-
-    dtotal = 0;
-    ptotal = 0;
-    const playerHand = [];
-    const dealerHand = [];
+    rebuild();
 
     for (let i = 0; i < 2; i++) {
-        const card = draw();
+        let card = draw();
         playerHand.push(card);
-        const dcard = draw();
+        let dcard = draw();
         dealerHand.push(dcard);
     }
 
     for (let i = 0; i < 2; i++) {
-        const imgElement = document.createElement("img");
+        let imgElement = document.createElement("img");
         if (i == 0) {
             dtotal += dealerHand[i].value;
             imgElement.src = dealerHand[i].front;
